60987062d2edb316975b7ad1e5631f5052683fa8
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2022-03-08 02:44:46 -0700

    Set up and example of running a 1D alchemical metadynamics simulation using gmxapi

---
+++ lambda_MetaD/lambda_MetaD.py
@@ -0,0 +1,18 @@
+import sys 
+sys.path.append('/home/wei-tse/gmxapi_21.4/lib/python3.7/site-packages')
+import gmxapi as gmx
+
+lambda_MetaD = gmx.commandline_operation('gmx',
+                              arguments=['mdrun', '-plumed', '../plumed.dat'],
+                              input_files={'-s': '../sys2_lambda.tpr'},
+                              output_files={
+                                  '-c': '../sys2_lambda.gro',
+                                  '-e': '../lambda.edr',
+                                  '-g': '../lambda.log',
+                                  '-o': '../lambda.trr'})
+lambda_MetaD.run()
+
+if lambda_MetaD.output.returncode.result() != 0:
+    print(f'STDERR of the process:\n\n {lambda_MetaD.output.stderr.result()}\n')
+else:
+    print('The process was executed successfully.')
